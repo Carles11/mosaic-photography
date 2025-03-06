ae29719dad2c097a0e4d7ff26627eeb53e6e363f
carlos <c.delriofrances@gmail.com>
2025-03-06 19:35:02 -0300

    updated import

--- src/components/modals/mainModal.tsx
+++ src/components/modals/mainModal.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import PrimaryButton from "@/components/buttons/primaryButton";
+import PrimaryButton from "@/components/buttons/PrimaryButton";
 import styles from "./mainModal.module.css";
 
 interface ModalProps {
