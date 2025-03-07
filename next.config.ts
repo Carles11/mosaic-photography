116f2b6754d5353198105e45192963f09c64e454
carlos <c.delriofrances@gmail.com>
2025-03-07 11:24:48 -0300

    updated header

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -15,4 +16,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  ...nextConfig,
+});
---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("mosaic-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/favicons/android-chrome-192x192.png",
+        "/favicons/android-chrome-512x512.png",
+        "/favicons/apple-touch-icon.png",
+        "/favicons/favicon-32x32.png",
+        "/favicons/favicon-16x16.png",
+        "/favicons/favicon.ico",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
