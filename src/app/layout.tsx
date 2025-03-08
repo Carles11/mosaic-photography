723e73e10c891403b88edf3a99c2225568146221
carlos <c.delriofrances@gmail.com>
2025-03-08 17:27:46 -0300

    pwa func fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import MetaHead from "@/components/metaHead";
-
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import { AppContextProvider } from "@/context/AppContext";
 import { ThemeProvider } from "next-themes";
 import GitHubCorner from "@/components/buttons/GitHubCorner";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -23,8 +22,33 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Mosaic Photography",
-  description: "Iconic nude photography from the Mosaic Gallery",
+  title: "Mosaic | Iconic Nude Photographers | Timeless Artistic Beauty",
+  description:
+    "Discover Mosaic's curated gallery of iconic nude photography, celebrating the beauty of the human form through the lens of the world's most renowned nude photographers.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  icons: {
+    icon: [
+      { rel: "icon", url: "/favicons/favicon.ico" },
+      {
+        rel: "icon",
+        url: "/favicons/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicons/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: "/favicons/apple-touch-icon.png",
+  },
+  manifest: "/favicons/site.webmanifest",
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({
@@ -34,7 +58,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <MetaHead />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider defaultTheme="system">
           <AppContextProvider>
